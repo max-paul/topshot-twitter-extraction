5bf9298d7089b6bd728632e92c1b9775b6c0333f
XamNalpak <75277029+XamNalpak@users.noreply.github.com>
2021-04-21 12:23:57 -0400

    final additions

--- query.sql
+++ query.sql
@@ -4,3 +4,9 @@
 select user_id,tweet
 from twitter_tweet
 where to_tsvector('english',tweet) @@ plainto_tsquery('english','TS')
+
+
+-- filter query for updating based on day!
+select user_id,tweet,TO_DATE(created_at,'DY Mon Year') as date
+from twitter_tweet
+where to_tsvector('english',tweet) @@ plainto_tsquery('english','TS') AND TO_DATE(created_at,'DY-Mon-DD HH24:MI:SS MS YYYY') = CURRENT_DATE 
